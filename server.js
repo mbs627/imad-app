6a7ca622ee9fe471c30ab3babf2df290c59df28d
mbs627 <35757791+mbs627@users.noreply.github.com>
2018-04-26 22:21:06 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ app.get('/', function (req, res) {
 });
 
 app.get('/a1', function (req, res){
-   res.send("hello thee");
+   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 
 app.get('/ui/style.css', function (req, res) {
