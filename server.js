2053a2fbe3bbc4509dbaa8a34ffa3f85814b2b00
mbs627 <35757791+mbs627@users.noreply.github.com>
2018-04-26 22:28:42 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -14,11 +14,11 @@ app.get('/a1', function (req, res){
 });
 
 app.get('/a2', function (req, res){
-   res.sendFile(path.join(__dirname, 'ui', 'a1.html'));
+   res.sendFile(path.join(__dirname, 'ui', 'a2.html'));
 });
 
 app.get('/a3', function (req, res){
-   res.sendFile(path.join(__dirname, 'ui', 'a1.html'));
+   res.sendFile(path.join(__dirname, 'ui', 'a3.html'));
 });
 
 app.get('/ui/style.css', function (req, res) {
